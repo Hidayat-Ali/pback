5bcc9b0e5dc7ec0da02873b17cc797e94fb00ff1
Hidayat-ali <meerhidayat563@gmail.com>
2023-11-22 20:00:14 +0530

    add:cors config

--- server.js
+++ server.js
@@ -9,24 +9,23 @@ const postRoute = require("./routes/posts");
 const cateRoute = require("./routes/categories");
 dotenv.config();
 const corsOptions = {
-  origin: "https://pfront.onrender.com",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: ["https://pfront.onrender.com", "http://localhost:4200"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true,
   optionsSuccessStatus: 204,
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
-const url =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://HidayatAli:hidayatdb121@cluster0.hqjmbvx.mongodb.net/?retryWrites=true&w=majority";
-
-const port = process.env.PORT || 3000;
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", cateRoute);
 console.log("Before MongoDB connection attempt");
+const url =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://HidayatAli:hidayatdb121@cluster0.hqjmbvx.mongodb.net/?retryWrites=true&w=majority";
+
+const port = process.env.PORT || 3000;
 mongoose
   .connect(url, {
     useNewUrlParser: true,
@@ -34,7 +33,7 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(3000, () => {
+    app.listen(port, () => {
       console.log("Backend is running");
     });
   })
